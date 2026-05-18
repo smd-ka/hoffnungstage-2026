0c77fea06b4cc5e2912659348891e2320cddb2bb
Felix Stupp <felix.stupp@banananet.work>
2026-05-18 10:10:36 +0000

    program/page: use correct createTranslator

--- src/routes/[lang=lang]/program/+page.svelte
+++ src/routes/[lang=lang]/program/+page.svelte
@@ -1,36 +1,41 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import type { TranslatedText } from '$lib/language';
+	import { createTranslator } from '$lib/language';
 	import ProgramCalendar from '../_components/ProgramCalendar.svelte';
 	import background_blended from '$lib/assets/pages/home/background_blended.jpg';
 	import ProgramList from '../_components/ProgramList.svelte';
 
 	$: lang = $page.params.lang as 'de' | 'en';
-
-	const translations = {
-		de: {
-			title: 'Programm',
-			description:
-				'Alle Veranstaltungen der Hoffnungstage vom 08. – 11. Juni 2026 auf einen Blick.',
-			languageNote: 'Für alle Vorträge und Formate werden auch englische Übersetzungen angeboten.',
-			clickForContent: 'Klicke auf einen Programmpunkt für mehr Details.',
-			preferList: 'Alternativ findest du hier alle Veranstaltungen aufgelistet:'
+	$: tr = createTranslator(
+		{
+			title: {
+				de: 'Programm',
+				en: 'Program'
+			},
+			description: {
+				de: 'Alle Veranstaltungen der Hoffnungstage vom 08. – 11. Juni 2026 auf einen Blick.',
+				en: 'All events of Hoffnungstage from June 8 – 11, 2026 at a glance.'
+			},
+			languageNote: {
+				de: 'Für alle Vorträge und Formate werden auch englische Übersetzungen angeboten.',
+				en: 'English translations will be provided for all talks and formats.'
+			},
+			clickForContent: {
+				de: 'Klicke auf einen Programmpunkt für mehr Details.',
+				en: 'Click on an event for more details.'
+			},
+			preferList: {
+				de: 'Alternativ findest du hier alle Veranstaltungen aufgelistet:',
+				en: 'Alternatively, you can find all events listed here:'
+			}
 		},
-		en: {
-			title: 'Program',
-			description: 'All events of Hoffnungstage from June 8-11, 2026 at a glance.',
-			languageNote: 'English translations will be provided for all talks and formats.',
-			clickForContent: 'Click on an event for more details.',
-			preferList: 'Alternatively, you can find all events listed here:'
-		}
-	};
-
-	$: t = translations[lang];
+		lang
+	);
 </script>
 
 <svelte:head>
-	<title>{t.title} | Hoffnungstage Karlsruhe</title>
-	<meta name="description" content={t.description} />
+	<title>{tr.title} | Hoffnungstage Karlsruhe</title>
+	<meta name="description" content={tr.description} />
 </svelte:head>
 
 <main
@@ -38,21 +43,21 @@
 	style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url({background_blended})"
 >
 	<header class="flex flex-col gap-4 px-4">
-		<h1 class="font-roman text-4xl font-bold lg:text-5xl">{t.title}</h1>
+		<h1 class="font-roman text-4xl font-bold lg:text-5xl">{tr.title}</h1>
 		<p>
-			{t.description}
+			{tr.description}
 			<br />
-			{t.languageNote}
+			{tr.languageNote}
 		</p>
 		<p>
-			{t.clickForContent}
+			{tr.clickForContent}
 		</p>
 	</header>
 	<div class="mx-auto max-w-6xl px-4 max-lg:hidden">
 		<ProgramCalendar />
 	</div>
 	<p class="max-lg:hidden">
-		{t.preferList}
+		{tr.preferList}
 	</p>
 	<div class="mx-auto max-w-6xl px-4">
 		<ProgramList />
