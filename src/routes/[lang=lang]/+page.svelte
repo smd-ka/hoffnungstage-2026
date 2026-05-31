93687121260d02e65de55579ac05968c7bd23275
Felix Stupp <felix.stupp@banananet.work>
2026-05-31 17:33:41 +0000

    add International Area to main page

--- src/routes/[lang=lang]/+page.svelte
+++ src/routes/[lang=lang]/+page.svelte
@@ -3,6 +3,7 @@
 	import Hero from './_components/Hero.svelte';
 	import HopeSection from './_components/HopeSection.svelte';
 	import ProgramSection from './_components/ProgramSection.svelte';
+	import InternationalAreaSection from './_components/InternationalAreaSection.svelte';
 	import AboutUsSection from './_components/AboutUsSection.svelte';
 	import ContactSection from './_components/ContactSection.svelte';
 </script>
@@ -15,6 +16,7 @@
 	<div class="flex flex-col gap-72 pb-72">
 		<HopeSection />
 		<ProgramSection />
+		<InternationalAreaSection />
 		<AboutUsSection />
 		<ContactSection />
 	</div>
---
+++ src/routes/[lang=lang]/_components/InternationalAreaSection.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { createTranslator } from '$lib/language';
+	import { locations } from '$lib/program/aux_data';
+
+	$: lang = $page.params.lang;
+
+	$: tr = createTranslator(
+		{
+			title: {
+				de: 'Internationale Area',
+				en: 'International Area'
+			},
+			desc: {
+				de: `
+					Die internationale Area ist der Ort der Hoffnungstage,
+					wo deutsche und internationale Studenten sich begegnen.
+					Ob beim kostenlosen Snack und Tee in gemütlicher Café-Atmosphäre oder beim Sport,
+					einfach Gemeinschaft haben und genießen.
+					Hier kannst du dein Deutsch oder Englisch verbessern,
+					egal wie gut dein Deutsch oder Englisch ist
+					– wir können beides.
+				`,
+				en: `
+					The International Area is the place at the Days of Hope
+					where German and international students come together.
+					Whether it\'s over free snacks and tea in a cozy café atmosphere or while playing sports,
+					it\'s simply about spend time together and enjoy it.
+					Here you can improve your German or English
+					no matter how good your German or English is
+					– we can speak both.
+				`
+			},
+			locationStart: {
+				de: 'Die internationale Area wird sich im Zelt ',
+				en: 'The International Area will be located in our tent '
+			},
+			locationDesc: locations['kit-forum-meadow'].sentenceEnd,
+			locationEnd: {
+				de: ' befinden.',
+				en: '.'
+			}
+		},
+		lang as 'de' | 'en'
+	);
+</script>
+
+<section class="page-section">
+	<h1>
+		{tr.title}
+	</h1>
+	<p>
+		{tr.desc}
+	</p>
+	<p>
+		{tr.locationStart}{tr.locationDesc}{tr.locationEnd}
+	</p>
+</section>
