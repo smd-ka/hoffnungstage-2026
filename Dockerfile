872aa3e759824f8ab48596fc67e467a05280ddbd
Felix Stupp <felix.stupp@banananet.work>
2026-06-01 09:44:21 +0000

    serve static MapLibre assets, add script to prepare those & add to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,27 @@
 FROM docker.io/library/node:lts-alpine AS base
 
 
+# get pmtiles binary
+FROM docker.io/protomaps/go-pmtiles AS go-pmtiles
+# prepare maplibre assets
+FROM docker.io/library/debian:stable AS maplibre-assets
+WORKDIR /app
+
+# download required packages for prepare_assets.sh
+RUN apt-get update && apt-get install -y --no-install-recommends\
+    ca-certificates \
+    curl \
+    unzip \
+    && rm -rf /var/lib/apt/lists/*
+
+# provide pmtiles binary
+COPY --from=go-pmtiles /go-pmtiles /usr/bin/pmtiles
+
+COPY ./prepare_assets.sh ./
+
+RUN ./prepare_assets.sh
+
+
 # Build stage
 FROM base AS build
 WORKDIR /app
@@ -16,7 +37,9 @@ ENV VITE_API_URL=$API_URL
 COPY ./package*.json ./
 RUN npm install
 
-# Copy all source files
+# Copy prepared assets
+COPY --from=maplibre-assets /app .
+# Copy all source files (overwrites any assets acquired by Dockerfile if they exist locally individually)
 COPY . .
 
 # Build the application
---
+++ prepare_assets.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+# This script downloads required assets which should NOT be commited.
+# The assets are stored so that Svelte / Vite automatically serves them
+# -> works for production docker image & your dev setup!
+
+set -euo pipefail
+
+
+# ==== CONFIGURATION ====
+TARGET_DIR="$PWD/static/maplibre"
+# for basemap-assets
+BASEMAPS_ASSETS_ZIP="https://github.com/protomaps/basemaps-assets/archive/refs/heads/main.zip"
+# for tiles
+PROTOMAPS_BUILD="https://build.protomaps.com/20260530.pmtiles"
+TILES_NAME="karlsruhe.pmtiles"
+TILES_BBOX="8.016586,48.853195,8.895493,49.158359" # same bbox as used throughout this project
+# pmtiles binary
+PMTILES_NIX_FLAKE_PKG="nixpkgs#pmtiles"
+PMTILES_DOCKER_IMAGE="docker.io/protomaps/go-pmtiles"
+
+
+# ==== TEMP SETUP ====
+TMPDIR="$(mktemp -d)"
+function cleanup {
+    rm -rf "$TMPDIR"
+}
+trap cleanup EXIT
+
+mkdir -p "$TARGET_DIR"
+
+
+set +x
+echo ">>> Downloading basemap-assets..."
+set -x
+
+ZIP_PATH="$TMPDIR/main.zip"
+curl -L "$BASEMAPS_ASSETS_ZIP" -o "$ZIP_PATH"
+unzip -q "$ZIP_PATH" -d "$TMPDIR"
+
+EXTRACTED_DIR="$TMPDIR/basemaps-assets-main"
+cp -r "$EXTRACTED_DIR/fonts" "$TARGET_DIR/"
+cp -r "$EXTRACTED_DIR/sprites" "$TARGET_DIR/"
+
+
+set +x
+echo ">>> Download & Generate tiles ..."
+set -x
+
+COMMON_ARGS=(
+    extract
+    "$PROTOMAPS_BUILD"
+    "$TILES_NAME"
+    --bbox="$TILES_BBOX"
+)
+
+if command -v pmtiles >/dev/null 2>&1; then
+    (cd "$TARGET_DIR" && pmtiles "${COMMON_ARGS[@]}")
+elif command -v nix >/dev/null 2>&1; then
+    (cd "$TARGET_DIR" && nix run nixpkgs#pmtiles -- "${COMMON_ARGS[@]}")
+elif command -v podman >/dev/null 2>&1; then
+    podman run --rm -v "$TARGET_DIR:/data" -w /data "$PMTILES_DOCKER_IMAGE" "${COMMON_ARGS[@]}"
+elif command -v docker >/dev/null 2>&1; then
+    docker run --rm -v "$TARGET_DIR:/data" -w /data "$PMTILES_DOCKER_IMAGE" "${COMMON_ARGS[@]}"
+else
+    echo "Error: need one of pmtiles, nix, podman, or docker installed." >&2
+    exit 1
+fi
+
+
+echo "Done."
