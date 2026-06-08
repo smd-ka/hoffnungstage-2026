4803b2fab43b3c701bf62bc1fd671dc912833d77
Felix Stupp <felix.stupp@banananet.work>
2026-06-08 09:00:27 +0000

    ProgramSection: reformat

--- src/routes/[lang=lang]/_components/ProgramSection.svelte
+++ src/routes/[lang=lang]/_components/ProgramSection.svelte
@@ -23,16 +23,42 @@
 				en: 'Discover more about the Events'
 			},
 			paragraph: {
-				de: 'Wir haben für dich ein spannendes und buntes Programm mit Vorträgen, Film, Konzert und Party mit DJ rund um das Thema Hoffnung vorbereitet. Begleitet von unserem Rahmenprogramm, das mit Volleyballfeld, Menschenkicker und Chillout-Area zum Verweilen und Genießen des Sommers einlädt.',
-				en: 'We’ve put together an amazing program for you, featuring talks, a film, a concert, and a party with a DJ, all centered around the topic of hope. Our supporting program includes a volleyball court, human foosball, and a chill-out area - perfect for relaxing and enjoying the summer.'
+				de: `
+					Wir haben für dich ein spannendes und buntes Programm
+					mit Vorträgen, Film, Konzert und Party mit DJ
+					rund um das Thema Hoffnung vorbereitet.
+					Begleitet von unserem Rahmenprogramm,
+					das mit Volleyballfeld, Menschenkicker und Chillout-Area
+					zum Verweilen und Genießen des Sommers einlädt.
+				`,
+				en: `
+					We’ve put together an amazing program for you,
+					featuring talks, a film, a concert, and a party with a DJ,
+					all centered around the topic of hope.
+					Our supporting program includes
+					a volleyball court, human foosball, and a chill-out area
+					- perfect for relaxing and enjoying the summer.
+				`
 			},
 			food: {
-				de: 'Auch für das leibliche Wohl ist gesorgt. Bei den meisten Veranstaltungen gibt es kostenlose Snacks und Getränke. Montag, Dienstag und Donnerstag gibt es am KIT sogar Mittagessen, an der PH gibt es immer Waffeln. Komm vorbei und genieße mit uns ☺️.',
-				en: 'We’ve got you covered when it comes to food and drinks. Most events offer free snacks and drinks. On Monday, Tuesday and Thursday, there’s even lunch at KIT, and there are always waffles at the PH. Come along and join us ☺️.'
+				de: `
+					Auch für das leibliche Wohl ist gesorgt.
+					Bei den meisten Veranstaltungen gibt es kostenlose Snacks und Getränke.
+					Montag, Dienstag und Donnerstag gibt es am KIT sogar Mittagessen,
+					an der PH gibt es immer Waffeln.
+					Komm vorbei und genieße mit uns ☺️.
+				`,
+				en: `
+					We’ve got you covered when it comes to food and drinks.
+					Most events offer free snacks and drinks.
+					On Monday, Tuesday and Thursday, there’s even lunch at KIT,
+					and there are always waffles at the PH.
+					Come along and join us ☺️.
+				`
 			},
 			languageNote: {
-				de: 'Hier im Kalender findest du alle Veranstaltungen der Hoffnungstage vom 08. bis 11. Juni 2026 auf einen Blick. Um mehr zu einer Veranstaltung zu erfahren, klicke einfach darauf. Für Vorträge und Formate am KIT werden auch mehrere Übersetzungen angeboten 🇬🇧, näheres findest du auf der Seite des jeweiligen Programmpunktes.',
-				en: 'Here in the calendar, you can see all the events taking place during the Days of Hope from 8 to 11 June 2026 at a glance. To find out more about an event, simply click on it. Several translations are also available for talks and sessions at KIT 🇬🇧; you can find more details on the page for each event.'
+				de: `Hier im Kalender findest du alle Veranstaltungen der Hoffnungstage vom 08. bis 11. Juni 2026 auf einen Blick. Um mehr zu einer Veranstaltung zu erfahren, klicke einfach darauf. Für Vorträge und Formate am KIT werden auch mehrere Übersetzungen angeboten 🇬🇧, näheres findest du auf der Seite des jeweiligen Programmpunktes.`,
+				en: `Here in the calendar, you can see all the events taking place during the Days of Hope from 8 to 11 June 2026 at a glance. To find out more about an event, simply click on it. Several translations are also available for talks and sessions at KIT 🇬🇧; you can find more details on the page for each event.`
 			}
 		},
 		lang as 'de' | 'en'
